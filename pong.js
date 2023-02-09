c108a724b6de974558fbcc6e02a7dd89524232ce
eskilja <eskil.janbu@gmail.com>
2023-02-09 08:55:50 +0100

    the start of pong

---
+++ pong.js
@@ -0,0 +1,61 @@
+var myGamePiece;
+        
+        function startGame() {
+            myGameArea.start();
+            myGamePiece = new component(15, 40, "red", 10, 120);
+        }
+        
+        var myGameArea = {
+            canvas : document.createElement("canvas"),
+            start : function() {
+                this.canvas.width = 480;
+                this.canvas.height = 270;
+                this.context = this.canvas.getContext("2d");
+                document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+                this.interval = setInterval(updateGameArea, 10);
+                //sjekker om en knapp har blitt trykket
+                window.addEventListener('keydown', function (e) {
+                    myGameArea.key = e.keyCode;
+                })
+                window.addEventListener('keyup', function (e) {
+                    myGameArea.key = false;
+                })
+            }, 
+            clear : function(){
+                this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+            }
+        }
+        
+        function component(width, height, color, x, y) {
+            this.gamearea = myGameArea;
+            this.width = width;
+            this.height = height;
+            this.speedX = 0;
+            this.speedY = 0;    
+            this.x = x;
+            this.y = y;    
+            this.update = function() {
+                ctx = myGameArea.context;
+                ctx.fillStyle = color;
+                ctx.fillRect(this.x, this.y, this.width, this.height);
+            }
+            this.newPos = function() {
+                this.x += this.speedX;
+                this.y += this.speedY;        
+            }
+        }
+        
+        function updateGameArea() {
+            //sletter alt på brettet
+            myGameArea.clear();
+            //setter default farten til 0
+            myGamePiece.speedX = 0;
+            myGamePiece.speedY = 0;    
+            //hvis den kanppen som er trykket er en av dusse to so vil den gå opp eller ned
+            if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
+            if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
+            //så vil den oppdatere posisjonen
+            myGamePiece.newPos();    
+            myGamePiece.update();
+        
+        }
