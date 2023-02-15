2e8b70331e2a18c183bcc6975b9885764f93ab54
eskilja <eskil.janbu@gmail.com>
2023-02-15 12:52:54 +0100

    lots of things are happening

--- flappy.js
+++ flappy.js
@@ -3,19 +3,22 @@ var myGamePiece;
         function startGame() {
             //kjører mygamearea
             myGameArea.start();
-            //sier hvordan boksen skal se ut
-            myGamePiece = new component(15, 50, "red", 20, 120);
-            //myGamePiece2 = new component(15, 50, "blue", 140, 120)
+            //sier hvordan boksen skal se ut og hvor den skal være
+            myGamePiece = new component(40, 40, "red", 40, 200);
+            
         }
         
         var myGameArea = {
+            //lager et dokument som heter canvas 
             canvas : document.createElement("canvas"),
+            //sier hva som skal skje når start skal skje 
             start : function() {
                 //hvor stor canvasen skal være
                 this.canvas.width = 480;
                 this.canvas.height = 480;
                 this.context = this.canvas.getContext("2d");
                 document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+                //sier at dokumentet skaal oppdateres hvert 10. sekund
                 this.interval = setInterval(updateGameArea, 10);
                 //sjekker om en knapp har blitt trykket
                 window.addEventListener('keydown', function (e) {
@@ -29,41 +32,55 @@ var myGamePiece;
                 this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
             }
         }
-        
+        //lager en funksjon som heter component
         function component(width, height, color, x, y) {
             this.gamearea = myGameArea;
             this.width = width;
             this.height = height;
-            this.speedX = 0;
-            this.speedY = 0;    
+            this.speedX = 0.0;
+            this.speedY = 0.0;    
             this.x = x;
             this.y = y;    
             this.update = function() {
                 ctx = myGameArea.context;
                 ctx.fillStyle = color;
-                ctx.fillRect(this.x, this.y, this.width, this.height);
+                ctx.fillRect(Math.floor(this.x), Math.floor(this.y), this.width, this.height);
             }
+            //lager en funksjon som heter newpos
             this.newPos = function() {
                 this.x += this.speedX;
-                this.y += this.speedY;        
+                this.y += this.speedY; 
+                //sier at hvis fulen treffer taket eller guvet 
+                //og sier hva som skal skje hvis den treffer 
+                if (this.y > 440){
+                    this.speedY = -0.8 * this.speedY;
+                    this.y = 440-(this.y-440);
+                }
+                if (this.y < 0) {
+                    this.speedY = -0.8*this.speedY
+                    this.y = -this.y;
+                }       
             }
         }
         
         function updateGameArea() {
             //sletter alt på brettet
             myGameArea.clear();
-            //setter default farten til 0
+            //setter farten til 0
             myGamePiece.speedX = 0;
-            myGamePiece.speedY = 0;    
+            myGamePiece.speedY = myGamePiece.speedY+0.1;    
             //hvis den kanppen som er trykket er en av dusse to so vil den gå opp eller ned
-            if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
-            if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
+            if (myGameArea.key && myGameArea.key == 38) {
+                if (myGamePiece.speedY > 0 ) {
+                    myGamePiece.speedY = 0;
+                }
+                myGamePiece.speedY = myGamePiece.speedY - 2; 
+                myGameArea.key = false;
+            }
             //så vil den oppdatere posisjonen
             myGamePiece.newPos();    
             myGamePiece.update();
         
         }
-        /*if (myGamePiece.x >= 270){
-            console.log('hit')
-        }*/
+        
         
--- testing2.js
+++ testing2.js
@@ -1,115 +1,110 @@
-const canvas = document.getElementById('canvas')
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-const circle = {
-    x: 200,
-    y: 200,
-    size: 30,
-    dx: 5,
-    dy: 4
-}
+const image = document.getElementById('source');
 
 const player = {
-    w: 50,
-    h: 70,
-    x: 20,
-    y: 200,
-    speed: 5,
-    dx: 0,
-    dy: 0,
-    color: red
+  w: 50,
+  h: 70,
+  x: 20,
+  y: 200,
+  speed: 10,
+  dx: 0,
+  dy: 0
+};
+
+function drawPlayer() {
+  ctx.drawImage(image, player.x, player.y, player.w, player.h);
 }
 
-function drawcircle(){
-    ctx.beginpath();
-    ctx.arc(circle.x, circle.y, cirlce.size, 0, Math.pi*2);
-    ctx.fillstyle = 'blue';
-    ctx.fill();
+function clear() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function drawplayer(){
-    ctx.drawImage(player.x, player.y, player.color, player.w, player.h);
+function newPos() {
+  player.x += player.dx;
+  player.y += player.dy;
 
+  detectWalls();
 }
 
-function clear(){
-    ctx.clearReact(0, 0, canvas.width, canvas.height);
+function detectWalls() {
+  // Left wall
+  if (player.x < 0) {
+    player.x = 0;
+  }
+
+  // Right Wall
+  if (player.x + player.w > canvas.width) {
+    player.x = canvas.width - player.w;
+  }
+
+  // Top wall
+  if (player.y < 0) {
+    player.y = 0;
+  }
+
+  // Bottom Wall
+  if (player.y + player.h > canvas.height) {
+    player.y = canvas.height - player.h;
+  }
 }
 
-function newPost(){
-    player.x += player.dx;
-    player.y += player.dy;
+function update() {
+  clear();
 
-    detectWalls();
+  drawPlayer();
+
+  newPos();
+
+  requestAnimationFrame(update);
 }
 
-function detectWalls(){
-    //opp
-    if(player.y<0){
-        player.y = 0;
-    }
-
-    //bunn
-    if(player.y + player.h > canvas.height) {
-        player.y = canvas.height - player.h;
-    }
+function moveUp() {
+  player.dy = -player.speed;
 }
 
-function update(){
-    //ctx.clearReact(0,0, canvas.Width, canvsa.height);
-
-    clear();
-    drawplayer();
-    newPost();
-
-    drawcircle();
-
-    //tegner på nytt
-    circle.x += circle.dx
-
-    //kolisjon sider
-    if(circle.x + circle.size > canvas.Width || circle.x - circle.size < 0) {
-        circle.dx *= -1 
-    }
-
-    //kolisjon top og bunn
-    if(circle.y +circle.size > canvas.height || circle.y - circle.size < 0){
-        circle.dy *-1;
-    }
-
-    requestAnimationFrame(update);
+function moveDown() {
+  player.dy = player.speed;
 }
 
-function moveUp(){
-    player.dy = -player.speed
+function moveRight() {
+  player.dx = player.speed;
 }
 
-function moveDown(){
-    player.dy = player.speed
+function moveLeft() {
+  player.dx = -player.speed;
 }
 
-function keydown(e){
-    if(e.key == 'ArrowUp'  || e.key == 'Up'){
-        moveUp();
-    } else if (e.key == 'ArrowDown' || e.key == 'Down'){
-        moveDown();
-    }
+function keyDown(e) {
+  if (e.key === 'ArrowRight' || e.key === 'Right') {
+    moveRight();
+  } else if (e.key === 'ArrowLeft' || e.key === 'Left') {
+    moveLeft();
+  } else if (e.key === 'ArrowUp' || e.key === 'Up') {
+    moveUp();
+  } else if (e.key === 'ArrowDown' || e.key === 'Down') {
+    moveDown();
+  }
 }
 
-function keyup (e) {
-    if(
-        e.key == 'Up' ||
-        e.key == 'ArrowUp' ||
-        e.key == 'Down' ||
-        e.key == 'ArrowDown' 
-    )
-    {
-        player.dx = 0;
-        player.dy = 0;
-    }
+function keyUp(e) {
+  if (
+    e.key == 'Right' ||
+    e.key == 'ArrowRight' ||
+    e.key == 'Left' ||
+    e.key == 'ArrowLeft' ||
+    e.key == 'Up' ||
+    e.key == 'ArrowUp' ||
+    e.key == 'Down' ||
+    e.key == 'ArrowDown'
+  ) {
+    player.dx = 0;
+    player.dy = 0;
+  }
 }
 
 update();
 
-document.addEventListener('keydown', keydown);
-document.addEventListener('keyup', keyup);
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
